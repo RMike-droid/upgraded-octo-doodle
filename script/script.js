cd36cfd443e97d770021ba93b1932f4119721be1
RMike-droid <galyenr@gmail.com>
2024-01-22 11:24:18 -0600

    Correct small syntax errors

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
-const selectionButtons = document.querySelectorAll('[data-selection')
+const selectionButtons = document.querySelectorAll('[data-selection]')
 const finalColumn = document.querySelector('[data-final-column]')
-const computerScoreSpan = document.querySelector('[data-computer-score')
-const yourScoreSpan = document.querySelector('[data-your-score')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
 const SELECTIONS = [
     {
         name: 'rock',
